e54ce0361e85da7ee9a9eb178dbb663a70d8596c
manasranjannayak <manasranjannayak123456@gmail.com>
2021-11-11 16:41:11 +0530

    update
    commite

---
+++ src/Lab2/q1/Exercise1.java
@@ -0,0 +1,35 @@
+package Lab2.q1;
+import java.util.Arrays;
+import java.util.Scanner;
+
+ class AlphabaticalSort {
+
+	public static void main(String[] args) {    // sort by alphabhet
+		AlphabaticalSort.sortStrings();
+	}
+
+	public static void sortStrings() {
+		Scanner sc = new Scanner(System.in);
+		Scanner sc1 = new Scanner(System.in);
+		// Declare array size
+		int n;
+		System.out.print("Enter number of elements : ");
+		// Initialize array size
+		n = sc.nextInt();
+
+		String arr[] = new String[n];
+		System.out.println("Enter the String : ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc1.nextLine();
+		}
+
+		Arrays.sort(arr); // Sort the array in alphabetical order
+
+		System.out.println(Arrays.toString(arr)); // Display the array
+		sc.close();
+		sc1.close();
+	}
+}
+public class Exercise1 {
+
+}
